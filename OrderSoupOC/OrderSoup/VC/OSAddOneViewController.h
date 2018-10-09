9ab9ad967efdfbc9dcbe747487d7452a06f6e0d2
Dawn <icy19882006@163.com>
2018-10-09 17:14:38 +0800

    init

---
+++ OrderSoupOC/OrderSoup/VC/OSAddOneViewController.h
@@ -0,0 +1,17 @@
+//
+//  OSAddOneViewController.h
+//  OrderSoupOC
+//
+//  Created by Dawn Wang on 2018/10/9.
+//  Copyright © 2018年 Dawn Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OSAddOneViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ OrderSoupOC/OrderSoup/VC/OSAddOneViewController.m
@@ -0,0 +1,32 @@
+//
+//  OSAddOneViewController.m
+//  OrderSoupOC
+//
+//  Created by Dawn Wang on 2018/10/9.
+//  Copyright © 2018年 Dawn Wang. All rights reserved.
+//
+
+#import "OSAddOneViewController.h"
+
+@interface OSAddOneViewController ()
+
+@end
+
+@implementation OSAddOneViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
